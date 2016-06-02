49aab83eeb9724b39afd3970ba82fbb9f1860b8b
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-06-02 16:59:03 +0700

    Create myApplicationComponent instant

--- app/src/main/java/com/silom313/sampledagger/MyApplication.java
+++ app/src/main/java/com/silom313/sampledagger/MyApplication.java
@@ -2,12 +2,15 @@ package com.silom313.sampledagger;
 
 import android.app.Application;
 
+import com.silom313.sampledagger.configuration.DaggerMyApplicationComponent;
+import com.silom313.sampledagger.configuration.MyApplicationComponent;
+
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-
+        MyApplicationComponent myApplicationComponent = DaggerMyApplicationComponent.create();
     }
 }
