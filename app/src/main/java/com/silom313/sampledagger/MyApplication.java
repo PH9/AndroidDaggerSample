e79a2e5fc30eedfd92531f6a72998318f853d15b
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-06-02 16:57:05 +0700

    Init: Override onCreate in MyApplication

--- app/src/main/java/com/silom313/sampledagger/MyApplication.java
+++ app/src/main/java/com/silom313/sampledagger/MyApplication.java
@@ -3,4 +3,11 @@ package com.silom313.sampledagger;
 import android.app.Application;
 
 public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
 }
