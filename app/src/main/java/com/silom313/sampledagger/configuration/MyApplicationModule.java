b1d81f422e0835ebd14bf71e2eca1dc5e2a80041
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-06-02 16:45:25 +0700

    Tell dagger how to create service

--- app/src/main/java/com/silom313/sampledagger/configuration/MyApplicationModule.java
+++ app/src/main/java/com/silom313/sampledagger/configuration/MyApplicationModule.java
@@ -1,7 +1,13 @@
 package com.silom313.sampledagger.configuration;
 
+import com.silom313.sampledagger.service.HelloWorldService;
+
 import dagger.Module;
 
 @Module
 public class MyApplicationModule {
+
+    HelloWorldService provideHelloWorldService() {
+        return new HelloWorldService();
+    }
 }
