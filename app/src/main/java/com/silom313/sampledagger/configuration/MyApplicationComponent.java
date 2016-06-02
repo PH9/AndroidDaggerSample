72ab2143bdf86bbb6329e84498a4fdb7c487499d
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-06-02 17:02:58 +0700

    Link Component between Application World and Dagger World

--- app/src/main/java/com/silom313/sampledagger/configuration/MyApplicationComponent.java
+++ app/src/main/java/com/silom313/sampledagger/configuration/MyApplicationComponent.java
@@ -1,7 +1,13 @@
 package com.silom313.sampledagger.configuration;
 
+import com.silom313.sampledagger.service.HelloService;
+
+import javax.inject.Singleton;
+
 import dagger.Component;
 
+@Singleton
 @Component(modules = MyApplicationModule.class)
 public interface MyApplicationComponent {
+    HelloService getHelloService();
 }
